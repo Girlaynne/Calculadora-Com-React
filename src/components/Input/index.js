2a23d4e8a8b2d7458aba0234c5904815a27a410c
Girlaynne <girlaynne.goncalves@gmail.com>
2024-10-15 19:23:07 -0300

    FirstCommit

---
+++ src/components/Input/index.js
@@ -0,0 +1,11 @@
+import { InputConteiner } from "./styles";
+
+const Input = ({value}) => {
+    return (
+      <InputConteiner>
+        <input disabled value={value}/>
+      </InputConteiner>
+    );
+  }
+  
+  export default Input;
---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const InputConteiner = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #6495ED;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #6495ED;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+    }
+`
